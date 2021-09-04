d09a5a349c302977e68599383b5612ab29287c75
Adem  Kurşun <adem.kursun@trendyol.com>
2021-09-04 17:27:01 +0300

    feat(loading): add loading route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Loading from "../views/Loading.vue";
+import Home from "../views/Home";
 
 Vue.use(VueRouter);
 
@@ -8,16 +9,17 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    component: Loading,
+  },
+  {
+    path: "/home",
+    name: "Home",
     component: Home,
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
 ];
 
