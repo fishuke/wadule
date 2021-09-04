c0e3b38a081954ad3e6e43ac2bda500db129d991
Adem  Kurşun <adem.kursun@trendyol.com>
2021-09-04 21:19:56 +0300

    feat(login): add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<script>
+import VueQr from "vue-qr";
+
+export default {
+  components: { VueQr },
+};
+</script>
+
+<template>
+  <div class="qr-container">
+    <vue-qr :text="this.$store.state.qr" />
+  </div>
+</template>
+
+<style>
+.qr-container {
+  @apply flex justify-center items-center w-full h-full;
+}
+</style>
