a07b15f7a21a432a59a40cc2bf11b22c6e4bb9dd
Fishuke <fishukee@gmail.com>
2021-09-27 02:24:27 +0300

    refactor: uncomment

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,23 @@
 </template>
 
 <script>
-// const electron = window.require("electron");
+const electron = window.require("electron");
 import store from "./store";
+import "./services/storage.service";
 
 export default {
   store: store,
-  // mounted: function () {
-  //   electron.ipcRenderer.on("qr", (event, qr) => {
-  //     this.$store.state.qr = qr;
-  //     this.$router.push("/login");
-  //   });
-  //   electron.ipcRenderer.on("ready", () => {
-  //     this.$router.push("/home");
-  //   });
-  // },
+  mounted: function () {
+    electron.ipcRenderer.on("qr", (event, qr) => {
+      this.$store.state.qr = qr;
+      this.$router.push("/login");
+    });
+    electron.ipcRenderer.on("ready", () => {
+      this.$router.push("/messages");
+    });
+    electron.ipcRenderer.on("contacts", (event, contacts) => {
+      this.$store.state.contacts = contacts;
+    });
+  },
 };
 </script>
